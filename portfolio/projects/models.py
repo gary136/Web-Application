2ba39b113116b02a123f8ad8a9beb88001fd9cd7
Gary <gary@hungdeair.mynetworksettings.com>
2023-11-11 23:19:36 -0500

    fix media path

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=20)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=70)
+    image = models.FileField(upload_to="project_images/", blank=True)
